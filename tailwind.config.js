986db90c0d519f6b4f77929eeca3643b133f8f6b
Kelvin Adjei <kyriejnr2610@gmail.com>
2025-12-18 21:39:01 +0000

    main screen design complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'light_main': "FFF2F0",
-        'light_sec': "F0E9E9",
-        'dark_main': "131324",
-        'dark_sec': "21214A"
+        'light_main': "#FFF2F0",
+        'light_sec': "#F0E9E9",
+        'dark_main': "#131324",
+        'dark_sec': "#21214A"
       },
       fontFamily: {
-        'bold': "",
         'primary': "Roboto"
       },
       animation: {
