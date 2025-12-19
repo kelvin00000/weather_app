ec340e8a3d7ca44d783ce93c40d2309fe29c4f43
Kelvin Adjei <kyriejnr2610@gmail.com>
2025-12-19 17:18:27 +0000

    dark mode design complete

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+////DARK MODE TOGGLE
+if (localStorage.getItem('theme') === 'dark') {
+  document.documentElement.classList.add('dark');
+}
+document.getElementById("change-theme-btn").addEventListener('click', ()=>{
+    document.documentElement.classList.toggle('dark');
+  
+    if (document.documentElement.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+})
+
+////SETTINGS POPUP TOGGLE
+document.getElementById("settings-open-btn").addEventListener('click', ()=>{
+    document.getElementById("settings-popup").classList.remove("popup-hidden");
+    document.getElementById("overlay").classList.remove("popup-hidden");
+})
+document.getElementById("settings-close-btn").addEventListener('click', ()=>{
+    document.getElementById("settings-popup").classList.add("popup-hidden");
+    document.getElementById("overlay").classList.add("popup-hidden");
+})
+
+
+
+
+
 // fetch("https://api.openweathermap.org/data/2.5/weather?q=kumasi&appid=a54398c98f1d1ee76bc81c14c54b576d")
 // .then(res => res.json())
 // .then(value => console.log(value))
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./*.html"],
   theme: {
     extend: {
       colors: {
         'light_main': "#FFF2F0",
-        'light_sec': "#F0E9E9",
+        'light_sec': "#E0D3D3",
         'dark_main': "#131324",
         'dark_sec': "#21214A"
       },
@@ -13,7 +14,7 @@ module.exports = {
         'primary': "Roboto"
       },
       animation: {
-
+        'spin-f': 'spin 0.5s linear infinite',
       }
     },
   },
